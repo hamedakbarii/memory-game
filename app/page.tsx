6db28b838f3c342403db0e8a8cbb36c4cd8208eb
hamedakbari <hamedakbariwork@gmail.com>
2025-05-09 17:11:06 +0330

    responsive added

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="w-full flex min-h-screen flex-col items-center justify-between">
       <MemoryGame />
-      <div>
+
+      <div className="my-10">
         Made with ❤️ by{" "}
         <Link
           href={"https://www.hamedakbri.ir"}
